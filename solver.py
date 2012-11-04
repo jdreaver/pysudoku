30415a416404eef34394171a0d12e1fad2f888ac
John David Reaver <jdreaver@adlerhorst.com>
2012-11-03 17:06:33 -0700

    logic solver is working

--- solver.py
+++ solver.py
@@ -1,3 +1,5 @@
+from load_sudokus import *
+
 # Sudoku solver
 
 def cross(A, B):
@@ -15,3 +17,50 @@ unitlist = rows + columns + squares
 
 units = dict((c, [unit for unit in unitlist if c in unit]) for c in cells)
 peers = dict((c, set(sum(units[c],[])) - set([c])) for c in cells)
+
+# Create puzzles
+
+puzzles = loadfile()
+
+def convert_puzzle(puzzle):
+    possibs = {}
+    for i, cell in enumerate(cells):
+        possibs[cell] = numbers if puzzle[i] == '0' else puzzle[i]
+    return possibs
+
+possibs = [convert_puzzle(puzzle) for puzzle in puzzles]
+
+def propagate_constraints(value, possibs, peers):
+    for peer in peers:
+        loc = possibs[peer].find(value)
+        if loc > -1:
+            possibs[peer] = possibs[peer][:loc] + possibs[peer][loc + 1:] 
+            
+
+def logic_solve(possibs):
+    unsolved_cells = [k for k in possibs.keys()]
+    num_changed = 1
+    while num_changed > 0:
+        num_changed = 0
+        for i, cell in enumerate(unsolved_cells):
+            value = possibs[cell]
+            if len(value) == 1:
+                del unsolved_cells[i]
+                propagate_constraints(value, possibs, peers[cell])
+                num_changed += 1
+    #return possibs
+    return search_solve(possibs, unsolved_cells) if unsolved_cells else possibs
+
+# Display function (stolen from Norvig)
+def display(values):
+    "Display these values as a 2-D grid."
+    width = 1+max(len(values[c]) for c in cells)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in letters:
+        print ''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in numbers)
+        if r in 'CF': print line
+    print
+
+
+
--- tests.py
+++ tests.py
@@ -1,10 +1,8 @@
 from solver import *
-from load_sudokus import *
 
 def test_suite():
     assert len(cells) == 81
     assert len(squares) == 9
 
-    puzzles = loadfile()
     assert len(puzzles) == 50
     assert len(puzzles[0]) == 81
